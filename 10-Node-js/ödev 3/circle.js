4fe4f0f94f4f4bed4738bd17b57744a328680599
Ömer <omergungorco@gmail.com>
2023-03-28 19:55:12 +0300

    Daire alan ve çevre modülleri yazıldı

---
+++ 10-Node-js/ödev 3/circle.js
@@ -0,0 +1,12 @@
+const circleArea = (r) => {
+    return Math.PI * r * r;
+}
+
+const circleCircumference = (r) => {
+    return 2 * Math.PI * r;
+}
+
+module.exports = {
+    circleArea,
+    circleCircumference
+}
